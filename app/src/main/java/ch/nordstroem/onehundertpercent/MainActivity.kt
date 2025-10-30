2e6f4ca1fd6a690cffdc2fa54ad295dcfc73f888
Kristoffer <kristoffer@nordstroem.ch>
2025-10-30 18:45:23 +0100

    100% of the current day seems to work

--- app/src/main/java/ch/nordstroem/onehundertpercent/MainActivity.kt
+++ app/src/main/java/ch/nordstroem/onehundertpercent/MainActivity.kt
@@ -1,27 +1,34 @@
 package ch.nordstroem.onehundertpercent
 
 import android.Manifest
-import android.os.Bundle
-import android.content.Context
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ch.nordstroem.onehundertpercent.ui.theme.OneHundertPercentTheme
+import java.util.Calendar
+import java.util.Date
+import kotlin.math.roundToInt
+
+val defaultLocation = Location(LocationManager.GPS_PROVIDER).apply {
+    latitude = 47.3769 // Zurich latitude
+    longitude = 8.5417 // Zurich longitude
+}
 
 class MainActivity : ComponentActivity() {
 
@@ -29,65 +36,97 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val defaultLocation = Location(LocationManager.GPS_PROVIDER).apply {
-                  latitude = 47.3769 // Zurich latitude
-                 longitude = 8.5417 // Zurich longitude
-        }
+
         var cLocation = defaultLocation;
 
-        var progress by remember { mutableStateOf(0) }
-
-        // Simulate progress update
-        LaunchedEffect(Unit) {
-            while (progress < 1.0f) {
-                progress += 10
-                delay(1000L) // Simulate work
-            }
-        }
+        var progress = 12
 
         setContent {
             OneHundertPercentTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        location = cLocation,
                         progress = progress,
-                        onProgressComplete = {
-                            Toast.makeText(this@MainActivity, "Progress Complete!", Toast.LENGTH_SHORT).show()
-                        },
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
         }
 
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         val locationListener = object : LocationListener {
             override fun onLocationChanged(location: Location) {
                 cLocation = location;
-                Toast.makeText(this@MainActivity, "A pikachu appeared nearby !" + location.latitude, Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this@MainActivity,
+                    "A pikachu appeared nearby !" + location.latitude,
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                setContent {
+                    OneHundertPercentTheme {
+                        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                            Greeting(
+                                location = cLocation,
+                                progress = progress,
+                                modifier = Modifier.padding(innerPadding),
+                            )
+                        }
+                    }
+                }
             }
         }
         // Request location updates (ensure you have the necessary permissions)
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener)
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            0L,
+            0f,
+            locationListener
+        )
     }
 }
 
 @Composable
-fun Greeting(name: String, progress: Int, onProgressComplete: () -> Unit, modifier: Modifier = Modifier) {
-    if (progress >= 100) {
-        onProgressComplete()
-    }
+fun Greeting(location: Location, progress: Int, modifier: Modifier = Modifier) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
+        val currentTime: Long = Calendar.getInstance().timeInMillis;
+        val startOfDay: Date = Calendar.getInstance().getTime()
+        startOfDay.hours = 0;
+        startOfDay.minutes = 0;
+        startOfDay.seconds = 0;
+
+        val delta = currentTime - startOfDay.toInstant().toEpochMilli();
+        val elapsedDay: Double = delta.toDouble() / (24.0 * 60 * 60 * 1000);
+
         CircularProgressIndicator(
-            progress = progress / 100f,
-            modifier = Modifier.size(100.dp)
+//      progress = { progress / 100f },
+            progress = { elapsedDay.toFloat() },
+            modifier = Modifier.size(240.dp),
+            strokeWidth = 30.dp,
+//        color = ProgressIndicatorDefaults.circularColor,
+//        trackColor = ProgressIndicatorDefaults.circularTrackColor,
+            strokeCap = StrokeCap.Butt,
         )
+
+        val percents: Float = (elapsedDay * 1000).roundToInt() / 10f
+
         Text(
-            text = "Hello $name!",
+            text = "${percents}%",
             modifier = modifier
+        )
+    }
+    Box(
+        contentAlignment = Alignment.TopEnd,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = ("Location \n"
+                    + "Lat ${location.latitude} N \n"
+                    + "Long ${location.longitude} W\n"
+                    )
         )
     }
 }
@@ -96,6 +135,9 @@ fun Greeting(name: String, progress: Int, onProgressComplete: () -> Unit, modifi
 @Composable
 fun GreetingPreview() {
     OneHundertPercentTheme {
-        Greeting("Android")
+        Greeting(
+            defaultLocation,
+            progress = 75
+        )
     }
 }
